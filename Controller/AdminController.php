594b1e6d1b3705c5d8b9d1ab2e77e10d486143fa
HakshoLOL <adri1du54@hotmail.fr>
2014-03-06 18:40:03 +0100

    cacher
    Signed-off-by: HakshoLOL <adri1du54@hotmail.fr>

--- Controller/AdminController.php
+++ Controller/AdminController.php
@@ -24,7 +24,9 @@ class AdminController extends Controller {
 	'modifierAccueil' => 'modifierAccueil',
 	'listDocuments' => 'listDocuments',
 	'supprimerDocument' => 'supprimerDocument',
-	'listCategories' => 'listCategories'
+	'supprimerCategorie' =>'supprimerCategorie',
+	'listCategories' => 'listCategories',
+	'cacherDocument' => 'cacherDocument'
     );
     static $allowedTags = "<div><p><h1><h2><h3><h4><h5><h6><ul><ol><li><dl><dt><dd><address><hr><pre><blockquote><center><ins><del><a><span><bdo><br><em><strong><dfn><code><samp><kbd><bar><cite><abbr><acronym><q><sub><sup><tt><i><b><big><small><u><s><strike><basefont><font><object><param><img><table><caption><colgroup><col><thead><tfoot><tbody><tr><th><td><embed>";
 
@@ -144,6 +146,19 @@ class AdminController extends Controller {
 	$view = new ModifierAccueilAdminView($document);
 	$view->displayPage();
     }
+    
+	// 1 cacher 2 montrer
+    public function cacherDocument() {
+	if (isset($_GET['id'])) {
+	    $id = $_GET['id'];
+	    if ($id != 1) {
+		$document = Document::findByID($id); 
+		$document->setAutorisation("9999/01/01");
+		$view = new OkAdminView("Le document est maintenant caché.");
+		$view->displayPage();
+	   }
+    	}
+    }		
 
     public function listDocuments() {
 	$documents = Categorie::findDocumentsByCategorie();
@@ -155,6 +170,26 @@ class AdminController extends Controller {
 	$categorie = Categorie::findAll();
 	$view = new ListCategoriesAdminView($categorie);
 	$view->displayPage();
+    }
+    
+    public function supprimerCategorie() {
+	if (isset($_GET['id'])) {
+	    $id = $_GET['id'];
+	    if ($id != 1) {
+		$cat = Categorie::findByID($id);
+		if ($cat == null) {
+		    $view = new ErrorAdminView("Catégorie non trouvée");
+		    $view->displayPage();
+		} else {
+		    $cat->delete();
+		    $view = new OkAdminView("La catégorie a bien été supprimée");
+		    $view->displayPage();
+		}
+	    } else {
+		$view = new ErrorAdminView("Imposssible de supprimer l'accueil");
+		$view->displayPage();
+	    }
+	}
     }
 
     public function supprimerDocument() {
--- View/ListCategoriesAdminView.php
+++ View/ListCategoriesAdminView.php
@@ -17,20 +17,26 @@ class ListCategoriesAdminView extends AdminView {
 	    <table>
 		<thead>
 		    <tr>
-			<th>Nom</th>
-			<th>Supprimer</th>
+			<th> Nom </th>
+			<th> Supprimer </th>
 		    </tr>
 		</thead>
 		<tbody>
 		    <?php
 		    foreach ($this->categories as $cat) :
+		    	if ($cat->getID() != 1) {
 		    	?>
-				<tr><td><?php echo $cat->getNom(); ?> </td></tr>
+
+				<tr><td><?php echo $cat->getNom(); ?> </td>
+				<td><a href="admin.php?a=supprimerCategorie&AMP;id=<?php echo $cat->getID()?>">Supprimer</a></td></tr>
+
 				<?php
+			}
 		    endforeach;
 		    ?>
 		</tbody>
 	    </table>
+	    <p> </br></br>Attention, supprimier une catégorie revient a supprimer l'intégralité des documents associés a celle-ci.</p>
 	</section> <?php
     }
 
